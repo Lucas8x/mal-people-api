fddc364ce7533a6b1623cdff59f5bb919953f06a
Lucas8x <lucas.poring99@gmail.com>
2020-07-30 11:09:13 -0300

    Init

---
+++ main.py
@@ -0,0 +1,34 @@
+from typing import Dict
+import requests
+from flask import Flask, jsonify
+from lxml import html
+
+app = Flask(__name__)
+
+
+def get_character_info(url: str) -> Dict[str, list]:
+  response = requests.get(f'{url}/pictures')
+  src = html.fromstring(response.content)
+  return {
+    'name': ''.join(src.xpath('//*[@class="h1-title"]/text()')).strip().replace("  ", " "),
+    'pictures': list(set(src.xpath('//*[@class="js-picture-gallery"]/@href')))
+  }
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def fetch(people_id: int):
+  print(f'Fetching people with id {people_id}')
+  response = requests.get(f'https://myanimelist.net/people/{people_id}')
+  src = html.fromstring(response.content)
+  characters_links = list(set(src.xpath('//*/td/a[contains(@href, "character")]/@href')))
+  characters = [get_character_info(url) for url in characters_links]
+  data = {
+    'name': ''.join(src.xpath('//*[@class="h1-title"]/text()')),
+    'characters': characters
+  }
+  print(f'Fetch > {people_id} - {data["name"]} < completed')
+  return jsonify(data)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3333)
