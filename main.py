880b68e5e3415b44bdef10e9968cf15307176664
Lucas8x <lucas.poring99@gmail.com>
2021-03-27 12:10:00 -0300

    :bug: fix people name & characters / fix character name & default

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict, Optional
+from typing import Dict, Optional, Any
 import requests
 from flask import Flask, jsonify, redirect
 from flask_cors import CORS
@@ -10,7 +10,7 @@ app.config['JSON_SORT_KEYS'] = False
 CORS(app)
 
 
-def get_page_src(url: str):
+def get_page_src(url: str) -> Optional[Any]:
   response = requests.get(url)
   src = html.fromstring(response.content)
   return src
@@ -19,27 +19,27 @@ def get_page_src(url: str):
 def get_character_info(url: str) -> Dict:
   src = get_page_src(f'{url}/pictures')
 
-  name = src.xpath('//*[@class="h1-title"]/text()')
+  name = src.xpath('//*[@class="h1-title"]//text()')
   default = src.xpath('//*[contains(@style, "text-align")]/a/img/@data-src')
   pictures = src.xpath('//*[@class="js-picture-gallery"]/@href')
 
   return {
     'name': ''.join(name).strip().replace('  ', ' '),
-    'default': default[0] if default else None,
+    'default': f'https://cdn.myanimelist.net{default[0]}' if default else None,
     'pictures': pictures if pictures else None
   }
 
 
 @app.route('/people/<int:people_id>')
-def fetch(people_id: int):
+def fetch(people_id: int) -> Any:
   print(f'Fetching people with id {people_id}')
   people_src = get_page_src(f'https://myanimelist.net/people/{people_id}')
   pictures_src = get_page_src(f'https://myanimelist.net/people/{people_id}/*/pictures')
 
-  characters_links = people_src.xpath('//*/td/a[contains(@href, "character")]/@href')
+  characters_links = people_src.xpath('//*/h3/a[contains(@href, "character")]/@href')
   characters_links = list(set(characters_links))
 
-  name = people_src.xpath('//*[@class="h1-title"]/text()')
+  name = people_src.xpath('//*[@class="h1-title"]//text()')
   default = people_src.xpath(f'//*[contains(@href, "{people_id}")]/img/@data-src')
   pictures = pictures_src.xpath('//*[@class="js-picture-gallery"]/@href')
   characters = [get_character_info(url) for url in characters_links]
@@ -55,7 +55,7 @@ def fetch(people_id: int):
 
 
 @app.route('/')
-def home():
+def home() -> None:
   return redirect('https://lucas8x.github.io/mal-va-mosaic/')
 
 
